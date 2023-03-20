97f157d7a1b4d8a7235a0a5df7ed70aefb9ee9a5
Elias Breitenbach <elias.b@me.com>
2023-03-20 21:51:02 +0100

    Pushed the project to github

---
+++ SteinScherrePapier/SteinScherrePapierApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteinScherrePapierApp.swift
+//  SteinScherrePapier
+//
+//  Created by Elias Breitenbach on 20.03.23.
+//
+
+import SwiftUI
+
+@main
+struct SteinScherrePapierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
