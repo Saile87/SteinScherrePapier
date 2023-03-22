13a1a7463939323de9012243933bd3e7a729b483
Elias Breitenbach <elias.b@me.com>
2023-03-22 22:20:41 +0100

    First attempts at coding

--- SteinScherrePapier/ContentView.swift
+++ SteinScherrePapier/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let choice = ["👊🏽", "✌🏽", "🫲🏽"].shuffled()
+    let win = ["Win", "Lose"].shuffled()
+    
+    @State private var currentChoice = false
+    @State private var loseOrWinv = false
+    @State private var weißNochNicht = Bool.random()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.1), location: 0.6),
+                .init(color: Color(red: 0.2, green: 0.1, blue: 0.2), location: 0.8),
+            ], center: .top, startRadius: 200, endRadius: 500)
+            .ignoresSafeArea()
+            VStack {
+                
+                Button("👊🏽") {
+                    
+                }
+                .font(.system(size: 200))
+                .shadow(radius: 9)
+            }
         }
-        .padding()
     }
 }
 
