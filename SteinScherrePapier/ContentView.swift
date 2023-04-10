83e4fad344f229a76f6b4507f2d7fae37759a70a
Elias Breitenbach <elias.b@me.com>
2023-04-10 16:41:36 +0200

    update

--- SteinScherrePapier/ContentView.swift
+++ SteinScherrePapier/ContentView.swift
@@ -9,48 +9,76 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var deineWahl = ["👊🏽", "✌🏽", "✋🏽"]
-    @State private var deineTextWahl = ["Stein", "Schere", "Papier"]
+    @State private var textWahl = ["Stein", "Schere", "Papier"]
     @State private var win = ["Win", "Lose"]
     @State private var controlingGameRound = false
-    @State private var currentChoice = false
     @State private var loseOrWin = false
-    @State private var SSP = Int.random(in: 0..<2)
+    @State private var randomSSP = Int.random(in: 0..<2)
     @State private var counter = 0
     @State private var scoreTitle = ""
+    @State private var score = 0
     
     var body: some View {
         ZStack {
             RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.1), location: 0.6),
-                .init(color: Color(red: 0.2, green: 0.1, blue: 0.2), location: 0.8),
+                
+                .init(color: Color(red: 0.0, green: 0.0, blue: 0.0), location: 0.6),
+                .init(color: Color(red: 0.0, green: 0.0, blue: 0.0), location: 0.8),
             ], center: .top, startRadius: 200, endRadius: 500)
             .ignoresSafeArea()
             VStack {
                 Spacer()
                 Spacer()
-                Text(deineWahl[SSP])
+                Text(deineWahl[randomSSP])
                 VStack {
-                    Text(deineTextWahl[SSP])
+                    Text(textWahl[randomSSP])
                         .foregroundColor(.white)
                 }
                 VStack {
-                    Button(loseOrWin ? (win[0]) : (win[1])) {
-                        loseOrWin.toggle()
-                    }
-                    .foregroundColor(loseOrWin ? .red : .green)
+                    Text(loseOrWin ? (win[0]) : (win[1]))
+                        .foregroundColor(loseOrWin ? .red : .green)
+                    Spacer()
+                    
+                    Text("Score \(score)")
+                        .foregroundColor(.white)
+                        .font(.title.bold())
+                    
                     Spacer()
                 }
                 HStack {
                     Button("👊🏽") {
+                        Game()
+                        loseOrWin.toggle()
+                        newRound()
                         self.counter += 1
-                        if self.counter >= 8 {
+                        if self.counter >= 44 {
                             self.controlingGameRound = true
+                            score = 0
+                            counter = 0
                         }
-                    
+                        
                     }
                     Button("✌🏽") {
+                        Game()
+                        loseOrWin.toggle()
+                        newRound()
+                        self.counter += 1
+                        if self.counter >= 44 {
+                            self.controlingGameRound = true
+                            score = 0
+                            counter = 0
+                        }
                     }
                     Button("✋🏽") {
+                        Game()
+                        loseOrWin.toggle()
+                        newRound()
+                        self.counter += 1
+                        if self.counter >= 44 {
+                            self.controlingGameRound = true
+                            score = 0
+                            counter = 0
+                        }
                     }
                 }
                 .padding()
@@ -67,17 +95,18 @@ struct ContentView: View {
             
         }
     }
-    func buttonTapped(_ number: Int) {
-        if number == SSP {
-            scoreTitle = "Correct"
-        } else if number != SSP {
-            scoreTitle = ("Wrong that's flag of:")
-        }
-        loseOrWin = true
+
+    func Game() {
+        if deineWahl == ["✋🏽"] {
+           
+                score += 1
+            } else {
+                score -= 1
+            }
+        
     }
     func newRound() {
-        deineWahl.shuffle()
-        SSP = Int.random(in: 0...2)
+        randomSSP = Int.random(in: 0...2)
     }
 }
 
